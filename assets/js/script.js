47cf199a65dc54e17e435df7552e0a57e2881954
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-06-29 15:04:50 -0400

    questions variable and array created

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+//variable questions added to create questions for the quiz
+var questions = [
+    {
+        title: "What city is the first US city to be founded by a woman?",
+        choices: ["Portland, Oregon", "Miami, Florida", "San Diego, California", "Trenton, NJ"],
+        answer: "Miami, Florida"
+    },
+    {
+        title: "What is the most visited attraction in the world?",
+        choices: ["The White House", "The Panama Canal", "Times Square, NYC", "the Roman Colosseum"],
+        answer: "the Roman Colosseum"
+    },
+    {
+        title: "What is the capital city of Spain?",
+        choices: ["Barcelona", "Seville", "Madrid", "Valencia"],
+        answer: "Madrid"
+    },
+    {
+        title: "Which country produces the most coffee in the world?",
+        choices: ["Brazil", "The United States", "Colombia", "Japan"],
+        answer: "Brazil"
+    },
+    {
+        title: "Zurich is the largest city in what country?",
+        choices: ["Germany", "Switzerland", "Austria", "Moldova"],
+        answer: "Switzerland"
+    },
+    {
+        title: "What is the world's biggest island?",
+        choices: ["Iceland", "Greenland", "Cuba", "Australia"],
+        answer: "Greenland"
+    },
+    {
+        title: "What is the national dish of Spain?",
+        choices: ["Paella", "Empanadas", "Tacos", "Flan"],
+        answer: "Paella"
+    },
+    {
+        title: "Which desert is the largest in the world?",
+        choices: ["The Mojave Desert", "The Great Basin", "The Atacoma Desert", "The Sahara Desert"],
+        answer: "The Sahara Desert"
+    },
+    {
+        title: "Which US city is known as the City of Brotherly Love?",
+        choices: ["New York, NY", "Philadephia, PA", "Sacramento, CA", "Trenton, NJ"],
+        answer: "Philadephia, PA"
+    },
+    {
+        title: "What is the capital city of Japan?",
+        choices: ["Yokohama", "Kyoto", "Tokyo", "Osaka"],
+        answer: "Tokyo"
+    },
+
+
+];
